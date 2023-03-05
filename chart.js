3b5272db05ef5399bc8adc3fb237dc44da9bda78
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2023-03-04 19:07:55 -0600

    API consume

--- chart.js
+++ chart.js
@@ -1,32 +1,37 @@
-const API =
-  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=mxn&order=market_cap_desc&per_page=100&page=1&sparkline=false";
-
-/* Creamos función para consumir API */
-const getCoins = async () => {
-  try {
-    const getApi = await fetch(API);
-    const data = await getApi.json();
-    console.log(data); //Nos ayuda a ver en consola
-    return data;
-  } catch (error) {
-    console.log("Error a la hora de consumir la API");
-  }
-};
-
-const dataAPI = getCoins();
-console.log(dataAPI);
-
 export const ctx = document.getElementById("myChart");
 
-export const miGrafica = () => {
+export const miGrafica = async () => {
+  const API =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=mxn&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+
+  /* Creamos función para consumir API */
+  const getCoins = async () => {
+    try {
+      const getApi = await fetch(API);
+      const data = await getApi.json();
+      console.log(data); //Nos ayuda a ver en consola
+      return data;
+    } catch (error) {
+      console.log("Error a la hora de consumir la API");
+    }
+  };
+
+  const dataAPI = await getCoins();
+  console.log(dataAPI);
+
   new Chart(ctx, {
     type: "bar",
     data: {
-      labels: ["Rojo", "Azul", "Amarillo", "Verde", "Morado", "Naranja"],
+      labels: [dataAPI[2].id, dataAPI[3].id, dataAPI[4].id, dataAPI[5].id],
       datasets: [
         {
           label: "Número de votos",
-          data: [12, 19, 3, 5, 2, 3],
+          data: [
+            dataAPI[2].current_price,
+            dataAPI[3].current_price,
+            dataAPI[4].current_price,
+            dataAPI[5].current_price,
+          ],
           borderWidth: 5,
         },
       ],
